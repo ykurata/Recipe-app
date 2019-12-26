3673d3b78363f9f4eb5616c8151849e780dc76f3
ykurata <yasukokurata@gmail.com>
2019-12-26 18:38:39 -0500

    cleaned code

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,13 +3,8 @@ import '../App.css';
 import landing from '../images/recipe-app-landing2.jpg';
 
 const Landing = () => {
-
     return (
         <div className="Landing" style={{backgroundImage: `url(${landing})` }}>
-            {/* <div className="Landing-content">
-                <h1>Pineapples</h1>
-                <p>They are good</p>
-            </div> */}
         </div>
     );
 
