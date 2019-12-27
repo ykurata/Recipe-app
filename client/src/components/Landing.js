7cfd1a59184a7b44b8bd686016a11a86aa75de8b
ykurata <yasukokurata@gmail.com>
2019-12-27 00:05:58 -0500

    transparent navbar

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../App.css';
 import landing from '../images/recipe-app-landing2.jpg';
+import Navbar from "./Navbar";
 
 const Landing = () => {
-    return (
-        <div className="Landing" style={{backgroundImage: `url(${landing})` }}>
-        </div>
-    );
-
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="Landing" style={{backgroundImage: `url(${landing})` }}></div>
+    </div>
+  );
 };
 
 export default Landing;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import '../App.css';
+
+const NavbarStyles = theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  } 
+  
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="appbar">
+        <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="default" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="default">Login</Button>
+          </Toolbar>
+        </AppBar>
+       </div>
+    );
+  }
+}
+
+export default withStyles(NavbarStyles)(Navbar);
