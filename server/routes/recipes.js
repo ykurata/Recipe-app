3af08dc1b5a68b8816f4558d68642baf6bce0d54
ykurata <yasukokurata@gmail.com>
2019-12-29 16:26:44 -0500

    add recipe rotues

---
+++ server/routes/recipes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Recipe = require("../models/Recipe");
+
+const auth = require("./middleware/utils");
+
+router.post("/", auth, (req, res, next) => {
+  const newRecipe = new Recipe({
+    userId: req.user,
+    name: req.body.name,
+    ingredients: req.body.ingredients,
+    steps: req.body.steps
+  });
+
+  newRecipe.save((err, recipe) => {
+    if (err) return next(err);
+    req.json(recipe);
+  });
+});
+
+module.exports = router;
