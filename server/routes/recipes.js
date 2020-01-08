eef3fb58d26486aad7b3aa325fe4542f1d1a01d7
ykurata <yasukokurata@gmail.com>
2020-01-08 17:17:09 -0500

    add put route

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Recipe = require("../models/Recipe");
-
 const auth = require("./middleware/utils");
 
+// Create a recipe 
 router.post("/", auth, (req, res, next) => {
   const newRecipe = new Recipe({
     userId: req.user,
@@ -17,5 +17,22 @@ router.post("/", auth, (req, res, next) => {
     res.json(recipe);
   });
 });
+
+
+// Update a recipe
+router.put("/update/:id", auth, (req, res, next) => {
+  Recipe.findOne({ _id: req.params.id }, (err, recipe) => {
+    if (err) return next(err);
+    recipe.name = req.body.name,
+    recipe.ingredients = req.body.ingredients,
+    recipe.steps = req.body.steps
+
+    recipe.save((err, recipe) => {
+      if (err) return next(err);
+      res.status(200).json(recipe);
+    });
+  });
+});
+
 
 module.exports = router;
