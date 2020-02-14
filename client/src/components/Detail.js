6279c7055a879045d44efbf3a93f628d693af81d
ykurata <yasukokurata@gmail.com>
2020-02-14 10:18:01 -0500

    add react-toastify

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Moment from 'react-moment';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Navbar from "./Navbar";
 
@@ -99,7 +102,10 @@ class Detail extends Component {
 
     axios.put(`/recipes/review/${this.props.match.params.id}`, newReview, { headers: { Authorization: `Bearer ${this.state.token}` }})
       .then(res => {
-        console.log(res.data);
+        toast.success("Successfully Submitted!" , {
+          position: "top-right",
+          autoClose: 10000
+        }); 
         this.setState({ review: "" });
         axios.get(`/recipes/get/${this.props.match.params.id}`, { headers: { Authorization: `Bearer ${this.state.token}` }})
           .then(res => {
@@ -114,7 +120,11 @@ class Detail extends Component {
           });
       })
       .catch(err => {
-        console.log(err.response.data);
+        console.log(err);
+        toast.error("Something went wrong", {
+          position: "top-right",
+          autoClose: 10000
+        });
       });
   };
 
@@ -124,102 +134,102 @@ class Detail extends Component {
 
     return (
       <div>
-       <Navbar></Navbar>
+        <Navbar></Navbar>
 
-       <div id="detail" className="main container-fluid padding">
-        <div className="inner-div">
-          <div className="col-12 text-center">
-            <h2 className="heading">{recipe.name}</h2>
-            <p>Created by {this.state.username}</p>
-          </div>
-        
-          <div className="row padding">
-            <div className="col-md-12 col-lg-6">
-              <div className="image text-center">
-                <img src={recipe.recipeImage} className="img-thumbnail img-fluid" alt="Recipe" />
-              </div>
+        <div id="detail" className="main container-fluid padding">
+          <div className="inner-div">
+            <div className="col-12 text-center">
+              <h2 className="heading">{recipe.name}</h2>
+              <p>Created by {this.state.username}</p>
             </div>
-            <div className="col-md-12 col-lg-6 text-center">
-              <div className="content">
-                <h5>Ingredients</h5>
-                <div className="card">
-                  <div className="card-body text-left">
-                    {recipe.ingredients}
-                  </div>
-                </div>
-              </div>
-              <div className="content">
-                <h5>Steps</h5>
-                <div className="card">
-                  <div className="card-body text-left">
-                    {recipe.steps}
-                  </div>
-                </div>
-              </div>
-
-              {/* Display Update & Delete buttons only for a user who created the recipe */}
-              {this.state.userId === this.state.userid ?
-                <div className="button-div">
-                  <a href={`/update/${recipe._id}`} type="button" className="btn btn-info">Update</a>
-                  <button 
-                    type="button" 
-                    className="delete btn btn-outline-info"
-                    onClick={(e) => { if (window.confirm('Are you sure you want to delete this recipe?')) this.deleteRecipe() } }
-                  >
-                    Delete
-                  </button>
-                </div>
-              : null  
-              }
-              
-              {/* Display Like & Write a Review buttons  */}
-              {this.state.userId !== this.state.userid ?
-                <div className="button-div text-center">
-                    <span className="likes-num">{this.state.likes}</span><i className="fas fa-heart icon" onClick={this.sendLike} type="button">Like</i>
-                    <i className="fas fa-pen icon reviewIcon" onClick={this.showInput}>Write a Review</i>
-                    {this.state.error ?
-                      <p className="error">{this.state.error}</p>
-                    : null  
-                    }
-                </div>
-              : null  
-              }
-            </div>
-          </div>
           
-          {/* Review input field */}
-          {this.state.show === true ?
-            <div className="col-12 text-center review">
-              <form onSubmit={this.sendReview}>
-                <textarea onChange={this.onChange} onClick={this.showButton} type="text" name="review" id="review" className="form-control" rows="2"></textarea>
-                {/* Show submit button by clicking textarea */}
-                {this.state.showButton === true ?
-                  <button type="submit" className="review-button btn btn-info">Submit</button>
+            <div className="row padding">
+              <div className="col-md-12 col-lg-6">
+                <div className="image text-center">
+                  <img src={recipe.recipeImage} className="img-thumbnail img-fluid" alt="Recipe" />
+                </div>
+              </div>
+              <div className="col-md-12 col-lg-6 text-center">
+                <div className="content">
+                  <h5>Ingredients</h5>
+                  <div className="card">
+                    <div className="card-body text-left">
+                      {recipe.ingredients}
+                    </div>
+                  </div>
+                </div>
+                <div className="content">
+                  <h5>Steps</h5>
+                  <div className="card">
+                    <div className="card-body text-left">
+                      {recipe.steps}
+                    </div>
+                  </div>
+                </div>
+
+                {/* Display Update & Delete buttons only for a user who created the recipe */}
+                {this.state.userId === this.state.userid ?
+                  <div className="button-div">
+                    <a href={`/update/${recipe._id}`} type="button" className="btn btn-info">Update</a>
+                    <button 
+                      type="button" 
+                      className="delete btn btn-outline-info"
+                      onClick={(e) => { if (window.confirm('Are you sure you want to delete this recipe?')) this.deleteRecipe() } }
+                    >
+                      Delete
+                    </button>
+                  </div>
                 : null  
                 }
-              </form>
-            </div>
-          : null  
-          }
-          
-          {/* Display reviews */}
-          {this.state.reviewLength !== 0 ? 
-            <div className="col-12 review">
-              <h5 className="title-review">Reviews ({this.state.reviewLength}) </h5>
-              {reviews.map((item, i) => 
-                <div className="card" key={i}>
-                  <div className="card-review">
-                    <span className="reviewer-name">{item.user.name} {item.createdAt}</span><br></br>
-                    {item.text}
+                
+                {/* Display Like & Write a Review buttons  */}
+                {this.state.userId !== this.state.userid ?
+                  <div className="button-div text-center">
+                      <span className="likes-num">{this.state.likes}</span><i className="fas fa-heart icon" onClick={this.sendLike} type="button">Like</i>
+                      <i className="fas fa-pen icon reviewIcon" onClick={this.showInput}>Write a Review</i>
+                      {this.state.error ?
+                        <p className="error">{this.state.error}</p>
+                      : null  
+                      }
                   </div>
-                </div>
-              )}
+                : null  
+                }
+              </div>
             </div>
-          : null  
-          }
-        
-        </div>
-      </div>  
+            
+            {/* Review input field */}
+            {this.state.show === true ?
+              <div className="col-12 text-center review">
+                <form onSubmit={this.sendReview}>
+                  <ToastContainer />
+                  <textarea onChange={this.onChange} onClick={this.showButton} type="text" name="review" id="review" className="form-control" rows="2"></textarea>
+                  {/* Show submit button by clicking textarea */}
+                  {this.state.showButton === true ?
+                    <button type="submit" className="review-button btn btn-info">Submit</button>
+                  : null  
+                  }
+                </form>
+              </div>
+            : null  
+            }
+            
+            {/* Display reviews */}
+            {this.state.reviewLength !== 0 ? 
+              <div className="col-12 review">
+                <h5 className="title-review">Reviews ({this.state.reviewLength}) </h5>
+                {reviews.map((item, i) => 
+                  <div className="card" key={i}>
+                    <div className="card-review">
+                      <span className="reviewer-name">{item.user.name} <Moment format="MM/DD/YYYY">{item.createdAt}</Moment></span><br></br>
+                      {item.text}
+                    </div>
+                  </div>
+                )}
+              </div>
+            : null  
+            }
+          </div>
+        </div>  
       </div>
     );
   };
