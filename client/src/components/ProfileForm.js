611c66d78580c302a2c25695a43e7a8e4f63ef68
ykurata <yasukokurata@gmail.com>
2020-02-24 12:39:29 -0500

    add send button in profile photo

--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -13,7 +13,6 @@ const ProfileFormStyle = theme => ({
   avatar: {
     width: 250,
     height: 250
-    
   }
 });
 
@@ -21,8 +20,21 @@ class ProfileForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      image: null,
+      sendImage: null,
+      description: ""
     };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  
+  imageChange = e => {
+    this.setState({
+      image: URL.createObjectURL(e.target.files[0]),
+      sendImage: e.target.files[0]
+    });
   }
 
   render() {
@@ -30,7 +42,7 @@ class ProfileForm extends Component {
     return (
       <div> 
         <Navbar></Navbar>
-        <div id="form">
+        <div id="profile-form">
           <div className="main container-fluid">
             <div className="col-12 text-center">
               <h2 className="heading">Edit Profile</h2>
@@ -38,25 +50,26 @@ class ProfileForm extends Component {
             <div className="row">
               <div className="col-md-12 col-lg-6">
                 <Grid container justify="center">  
-                  <Avatar className={classes.avatar}></Avatar>
+                  <Avatar className={classes.avatar} src={this.state.image}></Avatar>
                 </Grid>
-                <div className="text-center">
-                  <label className="btn btn-info">
+                <div className="text-center select">
+                  <label className="btn btn-outline-info select">
                     Select Image
                     <input
                       type="file"
                       name="image"
                       hidden
+                      onChange={this.imageChange}
                     />
                   </label>
                 </div>
-                <div className="text-center">
-                  <button className="btn btn-info">Submit</button>
+                <div className="text-center select">
+                  <button className="btn btn-info">Send</button>
                 </div> 
               </div>
               <div className="col-md-12 col-lg-6">
                 <form className="text-center border border-light">
-                  <textarea  className="form-control mb-4" name="ingredients" id="ingredients" rows="5" placeholder="Write about yourself..."></textarea>
+                  <textarea  onChange={this.onChange} className="form-control mb-4" name="description" id="description" rows="5" placeholder="Write about yourself..."></textarea>
                   <ToastContainer />
                   <button className="btn btn-info btn-block my-4" type="submit">Submit</button>
                   <a href="/"><p>Cancel</p></a>
