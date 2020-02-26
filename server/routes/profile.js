fbfe4d746aed9fe8045ec4b6074195cdc00154d9
ykurata <yasukokurata@gmail.com>
2020-02-26 16:00:05 -0500

    create POST profile photo route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 // Load input validation
 const validateProfileInput = require("../validation/profile");
@@ -7,6 +8,33 @@ const validateProfileInput = require("../validation/profile");
 // import model and auth middleware
 const Profile = require("../models/Profile");
 const auth = require("./middleware/utils");
+
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+
 
 // create a profile 
 router.post("/", auth, (req, res, next) => {
@@ -53,6 +81,35 @@ router.put("/update/:id", auth, (req, res, next) => {
         res.status(400).json(err);
       });
   });
+});
+
+
+router.post("/photo/:id", upload.single('profileImage'), auth, (req, res, next) => {
+  Profile.findOne({ userId: req.user }, function(profile, err){
+    if (err) return next(err);
+    if (profile) {
+      profile.photo = req.file.path;
+      profile.save()
+        .then(profile => {
+          res.status(200).json(profile);
+        })
+        .catch(err => {
+          res.status(400).json(err);
+        })
+    } else {
+      const newProfile = new Profile({
+        photo: req.file.path
+      })
+
+      newProfile.save()
+        .then(profile => {
+          res.status(200).json(profile);
+        })
+        .catch(err => {
+          res.status(400).json(err);
+        });
+    }
+  })
 });
 
 
