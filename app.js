bd82bc31fb2d2fd894953248ec913179b388a310
ykurata <yasukokurata@gmail.com>
2020-03-04 00:18:37 -0500

    make it better

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFild(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
