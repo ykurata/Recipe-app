cda1c40b783705b66356a8d8a5d28bd2e9e81065
ykurata <yasukokurata@gmail.com>
2020-03-08 13:45:34 -0400

    prepare for heroku

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const passport = require("passport");
 const cors = require("cors");
 const path = require("path");
 
-const users = require("./routes/users");
-const profile = require("./routes/profile");
-const recipes = require("./routes/recipes");
+const users = require("./server/routes/users");
+const profile = require("./server/routes/profile");
+const recipes = require("./server/routes/recipes");
 
 app.use(logger("dev"));
 
@@ -41,7 +41,7 @@ db.once("open", function(){
 app.use(passport.initialize());
 
 // Passport config
-require("./config/passport")(passport);
+require("./server/config/passport")(passport);
 
 // Routes
 app.use("/users", users);
