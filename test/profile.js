7d03536eed9c7c8f9f6262aa598eef674425c79d
ykurata <yasukokurata@gmail.com>
2020-06-05 11:28:51 -0400

    updated test files

--- test/profile.js
+++ test/profile.js
@@ -8,35 +8,35 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-// mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/recipe-api"); 
-// mongoose.connection
-//   .once('open', () => console.log('Connected!'))
-//   .on('error', (error) => {
-//       console.warn('Error : ',error);
-// });
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/recipe-api"); 
+mongoose.connection
+  .once('open', () => console.log('Connected!'))
+  .on('error', (error) => {
+      console.warn('Error : ',error);
+});
 
-// after("Disconnect database", (done) => {
-//     mongoose.connection.close().then(() => done()).catch(done);
-// });
+after("Disconnect database", (done) => {
+    mongoose.connection.close().then(() => done()).catch(done);
+});
 
 describe('Profiles', () => {
   let token;
   let userId;
-  // before((done) => {
-  //   chai.request(server)
-  //     .post('/users/login')
-  //     .send({
-  //       email: "yasuko@gmail.com",
-  //       password: 'testpassword'
-  //     })
-  //     .end((err, res) => {
-  //       if (err) throw err;
-  //       token = res.body.token;
-  //       const decode = jwt_decode(token);
-  //       userId = decode.id;
-  //       done();
-  //     });
-  // });
+  before((done) => {
+    chai.request(server)
+      .post('/users/login')
+      .send({
+        email: "yasuko@gmail.com",
+        password: 'testpassword'
+      })
+      .end((err, res) => {
+        if (err) throw err;
+        token = res.body.token;
+        const decode = jwt_decode(token);
+        userId = decode.id;
+        done();
+      });
+  });
 
   it('it should GET all the profiles', (done) => {
     chai.request(server)
@@ -48,45 +48,45 @@ describe('Profiles', () => {
         });
   });
 
-  // it("should GET a profile", (done) => {
-  //   chai.request(server)
-  //       .get(`/profile/${userId}`)
-  //       .set({ Authorization: `Bearer ${token}` })
-  //       .end((err, res) => {
-  //         res.should.have.status(200);
-  //         res.body.should.be.a('object');
-  //         res.body.should.have.property('description');
-  //         done();
-  //       });
-  // });  
+  it("should GET a profile", (done) => {
+    chai.request(server)
+        .get(`/profile/${userId}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('description');
+          done();
+        });
+  });  
 
-  // it("should POST a profile" ,(done) => {
-  //   chai.request(server)
-  //     .post(`/profile/${userId}`)
-  //     .set({ Authorization: `Bearer ${token}` })
-  //     .send({ 
-  //       description: "this is test"
-  //     })
-  //     .end((err, res) => {
-  //       res.should.have.status(200);
-  //       res.body.should.be.a('object');
-  //       res.body.should.have.property('description');
-  //       done()
-  //     });
-  // });
+  it("should POST a profile" ,(done) => {
+    chai.request(server)
+      .post(`/profile/${userId}`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({ 
+        description: "this is test"
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('description');
+        done()
+      });
+  });
 
-  // it("should update a profile" ,(done) => {
-  //   chai.request(server)
-  //     .put(`/profile/update/${userId}`)
-  //     .set({ Authorization: `Bearer ${token}` })
-  //     .send({ 
-  //       description: "this is test"
-  //     })
-  //     .end((err, res) => {
-  //       res.should.have.status(200);
-  //       res.body.should.be.a('object');
-  //       res.body.should.have.property('description');
-  //       done()
-  //     });
-  // });
+  it("should update a profile" ,(done) => {
+    chai.request(server)
+      .put(`/profile/update/${userId}`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({ 
+        description: "this is test"
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('description');
+        done()
+      });
+  });
 });
--- test/recipes.js
+++ test/recipes.js
@@ -9,35 +9,35 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-// mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/recipe-api"); 
-// mongoose.connection
-//   .once('open', () => console.log('Connected!'))
-//   .on('error', (error) => {
-//       console.warn('Error : ',error);
-// });
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/recipe-api"); 
+mongoose.connection
+  .once('open', () => console.log('Connected!'))
+  .on('error', (error) => {
+      console.warn('Error : ',error);
+});
 
-// after("Disconnect database", (done) => {
-//     mongoose.connection.close().then(() => done()).catch(done);
-// });
+after("Disconnect database", (done) => {
+    mongoose.connection.close().then(() => done()).catch(done);
+});
 
 describe('Recipes', () => {
   let token;
   let userId;
-  // before((done) => {
-  //   chai.request(server)
-  //     .post('/users/login')
-  //     .send({
-  //       email: "yasuko@gmail.com",
-  //       password: 'testpassword'
-  //     })
-  //     .end((err, res) => {
-  //       if (err) throw err;
-  //       token = res.body.token;
-  //       const decode = jwt_decode(token);
-  //       userId = decode.id; 
-  //       done();
-  //     });
-  // });
+  before((done) => {
+    chai.request(server)
+      .post('/users/login')
+      .send({
+        email: "yasuko@gmail.com",
+        password: 'testpassword'
+      })
+      .end((err, res) => {
+        if (err) throw err;
+        token = res.body.token;
+        const decode = jwt_decode(token);
+        userId = decode.id; 
+        done();
+      });
+  });
 
   it('should GET all the recipes', (done) => {
     chai.request(server)
@@ -49,25 +49,25 @@ describe('Recipes', () => {
         });
   });
 
-  // it("should GET logged in user's recipes", (done) => {
-  //   chai.request(server)
-  //       .get('/recipes/my-recipes')
-  //       .set({ Authorization: `Bearer ${token}` })
-  //       .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('array');
-  //         done();
-  //       });
-  // });
+  it("should GET logged in user's recipes", (done) => {
+    chai.request(server)
+        .get('/recipes/my-recipes')
+        .set({ Authorization: `Bearer ${token}` })
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+          done();
+        });
+  });
 
-  // it("should GET recipes by userId", (done) => {
-  //   chai.request(server)
-  //       .get(`/recipes/userid/${userId}`)
-  //       .set({ Authorization: `Bearer ${token}` })
-  //       .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('array');
-  //         done();
-  //       });
-  // });
+  it("should GET recipes by userId", (done) => {
+    chai.request(server)
+        .get(`/recipes/userid/${userId}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+          done();
+        });
+  });
 });
