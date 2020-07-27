0251e33afcb816daaa18d55fd1766d53a7d86898
ykurata <yasukokurata@gmail.com>
2020-07-26 23:58:12 -0400

    display profile image on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,14 +7,14 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [user, setUser] = useState({});
   const userId = localStorage.getItem("userId");
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("jwtToken");
 
   const toggleCollapse = () => {
     setIsOpen(!isOpen);
   }
   
   useEffect(() => {
-    axios.get(`/users/get/${userId}`, { headers: { Authorization: `Bearer ${token}` }})
+    axios.get(`/profile/${userId}`, { headers: { Authorization: `Bearer ${token}` }})
       .then(res => {
         setUser(res.data);
       })
@@ -23,52 +23,34 @@ const Navbar = () => {
       });
   }, []);
 
+  console.log(user)
+
   return (
     <MDBNavbar className="navbar" color="default-color" dark expand="md">
       <MDBNavbarBrand>
-        <strong className="white-text">My Recipes</strong>
+        <a href="/"><strong className="white-text">My Recipes</strong></a>
       </MDBNavbarBrand>
       <MDBNavbarToggler onClick={toggleCollapse} />
       <MDBCollapse id="navbarCollapse3" isOpen={isOpen} navbar>
         <MDBNavbarNav left>
           <MDBNavItem>
-            <MDBNavLink to="/">Home</MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink to="/search">Search</MDBNavLink>
+            <MDBNavLink to="/list">Search</MDBNavLink>
           </MDBNavItem>
           <MDBNavItem>
             <MDBNavLink to="/create">Create</MDBNavLink>
           </MDBNavItem>
           <MDBNavItem>
-            <MDBDropdown>
-              <MDBDropdownToggle nav caret>
-                <div className="d-none d-md-inline">Dropdown</div>
-              </MDBDropdownToggle>
-              <MDBDropdownMenu className="dropdown-default">
-                <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-              </MDBDropdownMenu>
-            </MDBDropdown>
+            <MDBNavLink to="/create">My Recipes</MDBNavLink>
           </MDBNavItem>
         </MDBNavbarNav>
         <MDBNavbarNav right>
           <MDBNavItem>
-            <MDBNavLink className="waves-effect waves-light" to="#!">
-              <MDBIcon fab icon="twitter" />
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink className="waves-effect waves-light" to="#!">
-              <MDBIcon fab icon="google-plus-g" />
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
             <MDBDropdown>
               <MDBDropdownToggle nav caret>
-                <MDBIcon icon="user" />
+                {user.photo ? 
+                  <img src={user.photo} alt="Avatar" className="md-avatar rounded-circle" />
+                : <MDBIcon icon="user" />     
+                } 
               </MDBDropdownToggle>
               <MDBDropdownMenu className="dropdown-default">
                 <MDBDropdownItem href="#!">Action</MDBDropdownItem>
