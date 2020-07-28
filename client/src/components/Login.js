ab3c1c8a657201a67a6bf8fae439a0e5de519558
ykurata <yasukokurata@gmail.com>
2020-07-28 15:27:49 -0400

    updated login and register page styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,102 +1,53 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import jwt_decode from "jwt-decode";
+import React from "react";
+import { 
+  MDBContainer, 
+  MDBRow, 
+  MDBCol, 
+  MDBInput, 
+  MDBBtn, 
+  MDBCard, 
+  MDBCardBody 
+} from 'mdbreact';
 
-import Navbar from "./Navbar";
-
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      validationErrors: [],
-      error: "",
-    };
-  }
-
-  // Update user input
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    const { email, password } = this.state;
-    const user = {
-      email: email,
-      password: password
-    };
-
-    axios.post("/users/login", user)
-      .then(res => {
-        const { token } = res.data;
-        const decoded = jwt_decode(token);
-        localStorage.setItem("jwtToken", token);
-        localStorage.setItem("name", decoded.name);
-        localStorage.setItem("userId", decoded.id);
-        this.props.history.push("/list");
-      })
-      .catch(err => {
-        this.setState({
-          validationErrors: err.response.data,
-          error: err.response.data.error
-        });
-      });
-  }
-
-  demoLogin = e => {
-    e.preventDefault();
-    const demoUser = {
-      email: "yasuko@gmail.com",
-      password: "testpassword"
-    };
-    axios.post("/users/login", demoUser)
-      .then(res => {
-        const { token } = res.data;
-        const decoded = jwt_decode(token);
-        localStorage.setItem("jwtToken", token);
-        localStorage.setItem("name", decoded.name);
-        localStorage.setItem("userId", decoded.id);
-        this.props.history.push("/list");
-      })
-      .catch(err => {
-        this.setState({
-          validationErrors: err.response.data,
-          error: err.response.data.error
-        });
-      });
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar></Navbar>
-        <div className="login-form">
-          <form className="text-center border border-light pt-5" onSubmit={this.onSubmit}>
-              <p className="h4 mb-4">Log in</p>
-              {this.state.error ? 
-                <p className="error">{this.state.error}</p>
-              : null}
-              {this.state.validationErrors ? 
-                <p className="error">{this.state.validationErrors.email}</p>
-              : null}
-              <input onChange={this.onChange} type="email" name="email" id="defaultLoginFormEmail" className="form-control mb-4" placeholder="E-mail" />
-              {this.state.validationErrors ? 
-                <p className="error">{this.state.validationErrors.password}</p>
-              : null}
-              <input onChange={this.onChange} type="password" name="password" id="defaultLoginFormPassword" className="form-control mb-4" placeholder="Password" />
-              <button className="btn btn-info btn-block my-4" type="submit">Log In</button>
-              <p>Not a member?
-                  <a href="/signup">Register</a>
-              </p>
-              <button className="btn btn-info" onClick={this.demoLogin}>Demo User</button>
-          </form>
-        </div>   
-      </div>
-    );
-  }
-}
+const Login = () => {
+  return (
+    <MDBContainer className="login-form">
+      <MDBRow>
+        <MDBCol>
+          <MDBCard className="login-card">
+            <MDBCardBody>
+              <form>
+                <p className="h4 text-center py-4">Log In</p>
+                <div className="grey-text">
+                  <MDBInput
+                    label="Your email"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Your password"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                </div>
+                <div className="text-center py-4 mt-3">
+                  <MDBBtn color="cyan" type="submit">
+                    Log In
+                  </MDBBtn>
+                </div>
+              </form>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
 
 export default Login;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,115 +1,69 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import jwt_decode from "jwt-decode";
+import React from "react";
+import { 
+  MDBContainer, 
+  MDBRow, 
+  MDBCol, 
+  MDBInput, 
+  MDBBtn, 
+  MDBCard, 
+  MDBCardBody 
+} from 'mdbreact';
 
-import Navbar from "./Navbar";
-
-class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      password2: "",
-      validationErrors: [],
-      error: ""
-    };
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    const { name, email, password, password2 } = this.state;
-    const newUser = {
-      name: name,
-      email: email,
-      password: password,
-      password2: password2
-    };
-
-    axios.post("/users/register", newUser)
-      .then(res => {
-        const { token } = res.data;
-        const decoded = jwt_decode(token);
-        localStorage.setItem("jwtToken", token);
-        localStorage.setItem("name", decoded.name);
-        localStorage.setItem("userId", decoded.id);
-        this.props.history.push("/list");
-      })
-      .catch(err => {
-        this.setState({
-          validationErrors: err.response.data,
-          error: err.response.data.error
-        });
-      });
-  }
-
-  demoLogin = e => {
-    e.preventDefault();
-    const demoUser = {
-      email: "yasuko@gmail.com",
-      password: "testpassword"
-    };
-    axios.post("/users/login", demoUser)
-      .then(res => {
-        const { token } = res.data;
-        const decoded = jwt_decode(token);
-        localStorage.setItem("jwtToken", token);
-        localStorage.setItem("name", decoded.name);
-        localStorage.setItem("userId", decoded.id);
-        this.props.history.push("/list");
-      })
-      .catch(err => {
-        this.setState({
-          validationErrors: err.response.data,
-          error: err.response.data.error
-        });
-      });
-  }
-
-  render() {
-   
-    return (
-      <div>
-        <Navbar></Navbar>
-        <div className="login-form">
-          <form className="text-center border border-light pt-5" onSubmit={this.onSubmit}>
-            <p className="h4 mb-4">Sign Up</p>
-            {this.state.error ? 
-              <p className="error">{this.state.error}</p>
-            : null}
-            {this.state.validationErrors ? 
-              <p className="error">{this.state.validationErrors.name}</p>
-            : null}
-            <input onChange={this.onChange} type="text" name="name" id="defaultRegisterFormName" className="form-control mb-4" placeholder="Name"></input>
-            {this.state.validationErrors ? 
-              <p className="error">{this.state.validationErrors.email}</p>
-            : null}
-            <input onChange={this.onChange} type="email" name="email" id="defaultLoginFormEmail" className="form-control mb-4" placeholder="E-mail" />
-            {this.state.validationErrors ? 
-              <p className="error">{this.state.validationErrors.password}</p>
-            : null}
-            <input onChange={this.onChange} type="password" name="password" id="password" className="form-control mb-4" placeholder="Password" />
-            {this.state.validationErrors ? 
-              <p className="error">{this.state.validationErrors.password2}</p>
-            : null}
-            <input onChange={this.onChange} type="password" name="password2" id="password2" className="form-control mb-4" placeholder="Confirm Password" />
-            <button className="btn btn-info btn-block my-4" type="submit">Sign Up</button>
-            <p>Already a member?
-                <a href="/login">Log In</a>
-            </p>
-            <button className="btn btn-info" onClick={this.demoLogin}>Demo User</button>
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
+const SignUp = () => {
+  return (
+    <MDBContainer className="login-form">
+      <MDBRow>
+        <MDBCol>
+          <MDBCard className="login-card">
+            <MDBCardBody>
+              <form>
+                <p className="h4 text-center py-4">Sign Up</p>
+                <div className="grey-text">
+                  <MDBInput
+                    label="Your name"
+                    icon="user"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Your email"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Your password"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                  <MDBInput
+                    label="Confirm Your password"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                </div>
+                <div className="text-center py-4 mt-3">
+                  <MDBBtn color="cyan" type="submit">
+                    Sign Up
+                  </MDBBtn>
+                </div>
+              </form>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
 
 export default SignUp;
-
