1f5ce5c8ff397eda6994843c345d6204349eab8d
ykurata <yasukokurata@gmail.com>
2020-08-10 14:23:02 -0400

    display saved recipe image on edit image page

--- client/src/pages/Image.js
+++ client/src/pages/Image.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { submitRecipeImage } from '../actions/recipeActions';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { submitRecipeImage, getRecipe } from '../actions/recipeActions';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -27,8 +28,19 @@ const Image = (props) => {
 	const imageChange = e => {
     setSendImage(e.target.files[0]);
 		setImage(URL.createObjectURL(e.target.files[0]));
-	}
-	
+  }
+  
+  // GET a recipe
+  useEffect(() => {
+    axios.get(`/recipes/get/${props.match.params.id}`)
+    .then(res => {
+      setImage(res.data.recipeImage);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+  
 	const onSubmit = e => {
 		e.preventDefault();
 
@@ -55,7 +67,7 @@ const Image = (props) => {
                       <p className="image-error">{error}</p>
                     : null
                   }
-                  {sendImage ? 
+                  {image ? 
                     <img src={image} alt="..." className="img-thumbnail" />
                   : 
                     <div className="no-image text-center">
