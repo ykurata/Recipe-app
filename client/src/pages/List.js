9ff56b47e66e4db271f6d5697cb6d0edfab91aed
ykurata <yasukokurata@gmail.com>
2020-08-11 11:21:13 -0400

    added search input on the list page

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getRecipes } from '../actions/recipeActions';
 
-import { MDBContainer } from 'mdbreact';
+import { 
+  MDBCol,
+  MDBContainer
+} from 'mdbreact';
 
 import Navbar from "../components/Navbar"
 import ListItems from "../components/ListItems";
 
 const List = () => {
+  const [search, setSearch] = useState("");
   const recipes = useSelector(state => state.recipe.recipes);
   const loading = useSelector(state => state.recipe.loading);
   const dispatch = useDispatch();
@@ -16,11 +20,34 @@ const List = () => {
     dispatch(getRecipes());
   }, []);
 
+  const onChange = e => {
+    setSearch(e.target.value);
+  }
+
+  // Filter by search input 
+  const filteredRecipes = recipes.filter(item => {
+    const query = search.toLowerCase();
+    return (
+      item.name.toLowerCase().indexOf(query) >= 0 ||
+      item.ingredients.toLowerCase().indexOf(query) >= 0 
+    )
+  });
+
   return (
     <div>
       <Navbar/>
+      <MDBCol md="6" className="search">
+        <input 
+          className="form-control" 
+          type="text" 
+          placeholder="Search by dish name or ingredients..." 
+          aria-label="Search" 
+          onChange={onChange}
+          value={search}
+        />
+      </MDBCol>
       <MDBContainer fluid className="list-container">
-        <ListItems data={recipes} loading={loading} />    
+        <ListItems data={filteredRecipes} loading={loading} />    
       </MDBContainer>
     </div>
   );
--- client/src/pages/MyRecipes.js
+++ client/src/pages/MyRecipes.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMyRecipes } from '../actions/recipeActions';
 
-import { MDBContainer } from 'mdbreact';
+import { 
+  MDBCol,
+  MDBContainer
+} from 'mdbreact';
 
 import Navbar from "../components/Navbar"
 import ListItems from "../components/ListItems";
 
 const MyRecipes = (props) => {
+  const [search, setSearch] = useState("");
   const token = localStorage.getItem("jwtToken");
   const dispatch = useDispatch();
   const recipes = useSelector(state => state.recipe.recipes);
@@ -17,11 +21,34 @@ const MyRecipes = (props) => {
     dispatch(getMyRecipes(token));
   }, []);
 
+  const onChange = e => {
+    setSearch(e.target.value);
+  }
+
+   // Filter by search input 
+   const filteredRecipes = recipes.filter(item => {
+    const query = search.toLowerCase();
+    return (
+      item.name.toLowerCase().indexOf(query) >= 0 ||
+      item.ingredients.toLowerCase().indexOf(query) >= 0 
+    )
+  });
+
   return (
     <div>
       <Navbar/>
+      <MDBCol md="6" className="search">
+        <input 
+          className="form-control" 
+          type="text" 
+          placeholder="Search by dish name or ingredients..." 
+          aria-label="Search" 
+          onChange={onChange}
+          value={search}
+        />
+      </MDBCol>
       <MDBContainer className="list-container">
-        <ListItems data={recipes} loading={loading} />
+        <ListItems data={filteredRecipes} loading={loading} />
       </MDBContainer>
     </div>
   );
