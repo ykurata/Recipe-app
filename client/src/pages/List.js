ce54c1e918c8ae81559be4bcb315a3761e3ce9ca
ykurata <yasukokurata@gmail.com>
2020-09-27 15:02:09 -0400

    updated search input

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getRecipes } from "../actions/recipeActions";
 
-import { MDBCol, MDBContainer, MDBInputGroup, MDBBtn } from "mdbreact";
+import { MDBCol, MDBContainer, MDBInputGroup, MDBBtn, MDBIcon } from "mdbreact";
 
 import Navbar from "../components/Navbar";
 import ListItems from "../components/ListItems";
@@ -21,8 +21,7 @@ const List = () => {
     setSearch(e.target.value);
   };
 
-  // Filter by search input
-  const filteredRecipes = recipes.filter((item) => {
+  let filteredRecipes = recipes.filter((item) => {
     const query = search.toLowerCase();
     return (
       item.name.toLowerCase().indexOf(query) >= 0 ||
@@ -33,26 +32,18 @@ const List = () => {
   return (
     <div>
       <Navbar />
+
       <MDBCol md="6" className="search">
-        <MDBInputGroup
-          hint="Dish name or Ingredients..."
-          containerClassName="mb-3"
-          onChange={onChange}
-          append={
-            <MDBBtn outline className="m-0 px-3 py-2 z-depth-0">
-              Search
-            </MDBBtn>
-          }
-        />
-        {/* <input
+        <input
           className="form-control"
           type="text"
           placeholder="Search by dish name or ingredients..."
           aria-label="Search"
           onChange={onChange}
           value={search}
-        /> */}
+        />
       </MDBCol>
+
       <MDBContainer className="list-container">
         <ListItems data={filteredRecipes} loading={loading} />
       </MDBContainer>
