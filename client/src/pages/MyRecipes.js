fad14768ac1faa542cc99a3d90ebf2a32b1a129a
ykurata <yasukokurata@gmail.com>
2020-10-01 13:05:04 -0400

    added category filter in the my-recipe page

--- client/src/pages/MyRecipes.js
+++ client/src/pages/MyRecipes.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMyRecipes } from '../actions/recipeActions';
+import { getCategories } from "../actions/categoryActions";
 
-import { 
+import {
   MDBCol,
   MDBContainer
 } from 'mdbreact';
@@ -16,37 +17,60 @@ const MyRecipes = (props) => {
   const dispatch = useDispatch();
   const recipes = useSelector(state => state.recipe.recipes);
   const loading = useSelector(state => state.recipe.loading);
+  const categories = useSelector(state => state.category.categories);
 
   useEffect(() => {
     dispatch(getMyRecipes(token));
+  }, []);
+
+  useEffect(() => {
+    dispatch(getCategories(token));
   }, []);
 
   const onChange = e => {
     setSearch(e.target.value);
   }
 
-   // Filter by search input 
-   const filteredRecipes = recipes.filter(item => {
+  // Filter by search input 
+  const filteredRecipes = recipes.filter(item => {
     const query = search.toLowerCase();
     return (
       item.name.toLowerCase().indexOf(query) >= 0 ||
-      item.ingredients.toLowerCase().indexOf(query) >= 0 
+      item.ingredients.toLowerCase().indexOf(query) >= 0 ||
+      item.category.toLowerCase().indexOf(query) >= 0
     )
   });
 
+  let options;
+  if (categories.length > 0) {
+    options = categories.map((item, index) =>
+      <option key={index} value={item.title}>{item.title}</option>
+    )
+  } else {
+    options = <option>No Options</option>
+  }
+
   return (
     <div>
-      <Navbar/>
-      <MDBCol md="6" className="search">
-        <input 
-          className="form-control" 
-          type="text" 
-          placeholder="Search by dish name or ingredients..." 
-          aria-label="Search" 
-          onChange={onChange}
-          value={search}
-        />
-      </MDBCol>
+      <Navbar />
+      <MDBContainer className="search-container">
+        <MDBCol lg="6" md="6" xs="12" className="search">
+          <select onChange={onChange} className="browser-default custom-select">
+            <option>Search by Category</option>
+            {options}
+          </select>
+        </MDBCol>
+        <MDBCol lg="6" md="6" xs="12" className="search search-input">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Search by dish name or ingredients..."
+            aria-label="Search"
+            onChange={onChange}
+            value={search}
+          />
+        </MDBCol>
+      </MDBContainer>
       <MDBContainer className="list-container">
         <ListItems data={filteredRecipes} loading={loading} />
       </MDBContainer>
